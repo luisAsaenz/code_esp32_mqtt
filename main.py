91a5f8a846587a2f0341c1c6c0d75d92a6e9a7c3
luisAsaenz <luis.a.saenz@outlook.com>
2025-04-22 14:36:12 -0700

    refactor UART message handling and button reading functionality

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# import modules
 # Derived from: 
 # * https://github.com/peterhinch/micropython-async/blob/master/v3/as_demos/auart.py
 # * https://github.com/tve/mqboard/blob/master/mqtt_async/hello-world.py
@@ -10,16 +11,240 @@ import ssl
 from mqtt_as.mqtt_as import MQTTClient
 from mqtt_as.mqtt_local import wifi_led, blue_led, config
 import uasyncio as asyncio
+from config import *
 from machine import UART
 from machine import Pin
 import time
-from config import *
 
-buttondebug = Pin(25,Pin.IN) # Button to SEND MESSAGE to MQTT broker
-MAXTX = 4
 
+MAX_MESSAGE_LEN=64
+team = [b'a',b'b',b'c',b'd']
+id = b'b' #tyler - c , alex - a, frank - d
+broadcast = b'X'
+
+# initialize a new UART class
 uart = UART(2, 9600,tx=17,rx=16)
-uart.init(9600, bits=8, parity=None, stop=1,flow=0) # init with given parameters
+# run the init method with more details including baudrate and parity
+uart.init(9600, bits=8, parity=None, stop=1) 
+# define pin 2 as an output with name led. (Pin 2 is connected to the ESP32-WROOM dev board's onboard blue LED)
+buttondebug = Pin(25,Pin.IN, Pin.PULL_UP) # Button to SEND MESSAGE to MQTT broker
+led = Pin(19,Pin.OUT) #change LED
+
+sensor_val = 0
+sensor_dict = {
+    '1': 0,
+    '2': 0
+}
+sensor_state = {
+    '1': 0,
+    '2': 0
+}
+MAXTX = 4
+received_sv = 0
+
+
+def send_message(message):
+    # if len(message)>MAX_MESSAGE_LEN:
+    #     print('ESP: message too long')
+    #     return
+    if message[0:2] != b'AZ':
+        print('ESP: message does not start with AZ')
+        return
+    if message[-2:] != b'YB':
+        print('ESP: message does not end with YB')
+        return
+    if message[2:3] not in team:
+        print('ESP: sender not in team')
+        return
+    if message[3:4] not in team:
+        if message[3:4] == broadcast:
+            print(f'ESP: sending broadcast message ---> {message}')
+            uart.write(message)
+
+            pass
+            
+        else:
+            print('ESP: receiver not in team')
+        return
+    
+    if message[2:3] == id:
+        uart.write(message)
+        print('ESP: sending MY message ', message)
+    else:
+        uart.write(message)
+        print('ESP: sending team message ', message)
+
+
+
+    # send_queue.append(message)
+
+def handle_message(message):
+    my_string = message.decode('utf-8')
+    if message[3:4] != id and message[3:4] != broadcast: ## checks if receiver is not my id or the broadcast
+       
+        print(f'ESP: handling team message {message}')
+        send_message(message)
+    else:
+        # if broadcast message and message type is 8 then pass message if not handle message normally then pass broadcast
+        message_type = message[4]
+        if message[3:4] == broadcast and message_type == 8:
+            print('ESP: passing broadcast ', message)
+            send_message(message)
+        else: 
+            if message[3:4] == broadcast:
+                print('ESP: handling broadcast ', message)
+            else:
+                print(f'ESP: handling my message {message}')
+            if message_type == 2:
+                # Handle message type 2
+                sensor_id = my_string[5]
+                sensor_value = message[6]
+                if sensor_id not in sensor_dict.keys():
+                    print('ESP: sensor id is invalid. Try, 1 or 2.')
+                    sensor_value = 0
+                else:
+                    if sensor_value > 100:
+                        print('ESP: sensor value is too out of range')
+                        sensor_value = 0
+                    else:
+                        print(f'ESP: message contains sensor {sensor_id} value: {sensor_value}')
+                        sensor_dict[sensor_id] = sensor_value
+                        sensor_state[sensor_id] = 1 
+
+
+                pass
+
+            elif message_type == 5:
+                # Handle message type 5
+                subsystem_id = my_string[5]
+                if subsystem_id in team.decode('utf-8'):
+                    print(f'ESP: subsystem {subsystem_id} that is experiencing error')
+                else:
+                    print(f'ESP: Subsystem, {subsystem_id} is not in team.')
+                pass
+
+            elif message_type == 6:
+                # Handle message type 6
+                motor_status = message[5]
+                if motor_status == 0:
+                    print('ESP: Motor is not down.')
+                elif motor_status == 1:
+                    print('ESP: Motor is up.')
+                else: 
+                    print('ESP: Motor status is unknown.')
+
+            elif message_type == 7:
+                # Handle message type 7
+                sensor_status = message[5]
+                if sensor_status == 0:
+                    print('ESP: Sensor is not down.')
+                elif sensor_status == 1:
+                    print('ESP: Sensor is up.')
+                else: 
+                    print('ESP: Sensor status is unknown.')
+                pass
+
+            else:
+                print('ESP: unknown message type.')
+            if message[3:4] == broadcast:
+                print('ESP: broadcast messaged handled, passing broadcast.')
+                send_message(message)
+            else:
+                print(f'ESP: Message, {message} ,  handled. Deleting message..')  
+
+    
+
+
+async def process_rx():
+
+    # stream = []
+    stream = b''
+    message = b''
+    send_queue = []
+    receiving_message=False
+
+    while True:
+        # read one byte
+        c = uart.read(1)
+        # if c is not empty:
+        
+            
+        if c is not None:
+            
+            stream+=c
+            try:
+                if stream[-2:]==b'AZ':
+                    # print('ESP: message start:')
+                    message=stream[-2:-1] #add a to message
+                    receiving_message=True
+            except IndexError:
+                pass
+            try:
+                if stream[-2:]==b'YB':
+                    if receiving_message == False:
+                        message=b''
+                        pass
+                    else:
+                        message+=stream[-1:]
+                        receiving_message = False
+                        print('ESP: message received:',message)
+                        handle_message(message)
+                    stream=b''
+
+                    led.value(led.value()^1)
+            
+            except IndexError:
+                pass
+            #try:
+            #   this will handle incoming messages that are from MQTT
+            #except IndexError:
+            #    pass
+            if receiving_message:
+                
+                message+=c #immediately after receiving_message == true, Z is added to message
+                #print(message) 
+                #print(c)
+
+
+                if len(message)==3:
+                    if  (message[2:3] not in team):
+                        print('ESP: sender not in team ------>  ')
+                        print(c)
+
+                        # get rid of message if sender not on team
+                       # message=b''
+                        receiving_message = False
+                    else:
+                        if message[2:3] == id:
+                            print('ESP: receiving message from self. DELETING...')
+                          #  message=b''
+                            receiving_message = False
+                        else:
+                            print('ESP: sender in team')
+
+                if len(message)==4:
+                    if  (message[3:4] not in team):
+                        
+                        # get rid of message if sender not on team
+                        #message=b''
+                        if message[3:4] == broadcast:
+                            print('ESP: receiving broadcast')
+                        else:
+                            print('ESP: receiver not in team')
+                            receiving_message = False
+                    else:
+                        print('ESP: receiver in team')
+
+                if len(message)>MAX_MESSAGE_LEN:
+                    receiving_message = False
+                    print('ESP: Message too long, aborting')
+                    message=b''
+
+
+        await asyncio.sleep_ms(10)    
+
+
+
 
 async def readingButton():
     laststate = 0
@@ -39,26 +264,60 @@ async def readingButton():
         pass
 
 
-async def receiver():
-    b = b''
-    sreader = asyncio.StreamReader(uart)
-    while True:
-        res = await sreader.read(1)
-        if res==b';':
-            b+=res
-            await client.publish(TOPIC_PUB, b, qos=1)
-
-            print('published', b)
-            b = b''
-        else:
-            b+=res
-
 # Subscription callback
 def sub_cb(topic, msg, retained):
 
     print(f'Topic: "{topic.decode()}" Message: "{msg.decode()}" Retained: {retained}')
+    
+    if topic.decode()[-3:] == 'SUB':
+        # send message to frank
+        try:
+            s = b''
+            if msg[-1:] != b';':
+                await client.publish(TOPIC_PUB, b'End message in ";"', qos=1)
+                return
+            else:
+                if len(msg) > 3:
+                    await client.publish(TOPIC_PUB, b'Entry too long. Try values: 0, 1, 2. Followed by ";"', qos=1)
+                    return
+                else:
+                    if len(msg) < 3:
+                        await client.publish(TOPIC_PUB, b'Please enter a value. Try values: 0, 1, 2. Followed by ";"', qos=1)
+                        return
+                    if msg[1:2] not in b'012':
+                        if type(msg[1:2]) != bytes:
+                            await client.publish(TOPIC_PUB, b'Use regular characters when entering values. Try values: 0, 1, 2. Followed by ";"', qos=1)
+                            return
+                        else:
+                            await client.publish(TOPIC_PUB, b'Entry unaccepted. Try values: 0, 1, 2. Followed by ";"', qos=1)
+                            return
+                #now that error handling is done. we check once again if msg is in index of acceptable values
+                if msg[1:2] in b'012':
+                    s = b'AZbd\x011'+ msg[1:2] + b'YB'
+                    await client.publish(TOPIC_PUB, msg, qos=1)
 
-    uart.write(msg)
+                    send_message(s)
+        except IndexError:
+            pass
+
+async def sensor_update():
+    while True:
+        await asyncio.sleep_ms(100)
+        if sensor_state['1'] and sensor_state['2'] == 0:
+            sensor_vals = 'The value of sensor 1: not connected. Sensor 2: not connected'
+        elif sensor_state['1'] or sensor_state['2'] == 0:
+            if sensor_state['1'] == 1:
+                sensor_vals = 'The value of sensor 1: {}. Sensor 2: not connected'.format(sensor_dict['1'])
+            else:
+                sensor_vals = 'The value of sensor 1: not connected Sensor 2: {}'.format(sensor_dict['2'])
+
+        else:
+            sensor_vals = 'The value of sensor 1: {}. Sensor 2: {}'.format(sensor_dict['1'], sensor_dict['2'])
+         
+
+        await client.publish(TOPIC_SENSOR, sensor_vals, qos=1)
+
+
     # time.sleep(.01)
 
 
@@ -70,14 +329,20 @@ async def heartbeat():
         blue_led(s)
         s = not s
 
+
+
 async def wifi_han(state):
     wifi_led(not state)
     print('Wifi is ', 'up' if state else 'down')
     await asyncio.sleep(1)
 
+
+
 # If you connect with clean_session True, must re-subscribe (MQTT spec 3.1.2.4)
 async def conn_han(client):
     await client.subscribe(TOPIC_SUB, 1)
+
+
 
 async def main(client):
     try:
@@ -85,8 +350,10 @@ async def main(client):
     except OSError:
         print('Connection failed.')
         return
-    asyncio.create_task(receiver())
+    ##asyncio.create_task(process_rx())
     asyncio.create_task(readingButton())
+    asyncio.create_task(sensor_update())
+
 
 
     n = 0
@@ -95,7 +362,44 @@ async def main(client):
         print('publish', n)
         # If WiFi is down the following will pause for the duration.
         await client.publish(TOPIC_HB, '{} {}'.format(n, client.REPUB_COUNT), qos = 1)
+
         n += 1
+    #counter = 0
+
+    #while True:
+    #    token = counter%100
+    #    
+    #    if token == 9: # MESSAGE TYPE1
+    #        message = b'AZcd\x0130dfdsgrejhv dfjhgdlkfjhvldkfj fjklhguieoeh dfhuigb dfjfdgfdgsdfgdfgdffYB'
+    #        send_message(message)
+    #
+    #    if token == 11: # MESSAGE TYPE3
+    #        message = b'AZbX\x03wifi is not communicatingYB'       
+    #        send_message(message)
+#
+    #    if token == 15: # MESSAGE TYPE2
+    #        message = b'AZcb\x021\x10YB'
+    #        send_message(message)
+#
+    #    if token == 22: # MESSAGE TYPE6
+    #        message = b'AZcX\x08this is broadcastYB'
+    #        send_message(message)
+
+        # if token == 40: # MESSAGE TYPE4
+        #     message = b'AZcb\x040YB'
+        #     send_message(message)
+
+        # if token ==  45: # MESSAGE TYPe5
+        #     message = b'AZbd\x05bYB'
+        #     send_message(message)
+
+        # if token == 50: # MESSAGE TYPe8
+        #     message = b'AZcX\x08this is the broadcastYB'
+        #     send_message(message)
+        
+
+        #counter += 1
+        #await asyncio.sleep(1)
 
 # Define configuration
 
@@ -133,6 +437,7 @@ config["password"] = MQTT_PASSWORD
 MQTTClient.DEBUG = True  # Optional
 client = MQTTClient(config)
 
+asyncio.create_task(process_rx()) #if don't work remove and add to main function
 asyncio.create_task(heartbeat())
 
 try:
